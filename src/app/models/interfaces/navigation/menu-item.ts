4803d83721baad64426c1d1f30d2d0a5495514d1
Thomas Adams <heinrich.adams@gmail.com>
2019-07-11 20:29:14 +0200

    fixing accidentally removed components

--- src/app/models/interfaces/navigation/menu-item.ts
+++ src/app/models/interfaces/navigation/menu-item.ts
@@ -1,10 +1,11 @@
 export interface MenuItem {
-  id: any;
+  id: string;
   title?: string;
   name?: string;
   sort?: any;
   link?: string;
   level?: number;
+  attributes?: { [key: string]: any };
   children?: Array<MenuItem>;
   parentId?: string | null;
   parentName?: string | null;
