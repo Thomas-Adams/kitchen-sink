2b40cf4e08e8f3b98d9b00948f24cc42325b6549
Thomas Adams <heinrich.adams@gmail.com>
2019-07-11 21:02:58 +0200

    fixing accidentally removed components

--- src/app/functions/navigation/convert-node-data-to-menu-item.spec.ts
+++ src/app/functions/navigation/convert-node-data-to-menu-item.spec.ts
@@ -1,11 +1,7 @@
-import menuData from '../../../assets/data/menu-01/countries.json';
-import {MenuItem} from '../../models/interfaces/navigation/menu-item';
-import {filterTreeByAttribute} from './filter-tree-by-attribute';
 import nodeData from '../../../assets/data/menu-02/countries.json';
 import {convertNodeDataToMenuItem} from './convert-node-data-to-menu-item';
 
 fdescribe('Convert "NodeData" to "MenuItem"', () => {
-    const menu = menuData as MenuItem;
     fit('Converting to "MenuItem" should give the right id property', () => {
         const menuItem = convertNodeDataToMenuItem(nodeData);
         expect(menuItem.id).toBe('-1');
@@ -23,6 +19,6 @@ fdescribe('Convert "NodeData" to "MenuItem"', () => {
 
     fit('Converting to "MenuItem" should give the child as Asia', () => {
         const menuItem = convertNodeDataToMenuItem(nodeData);
-        expect(menuItem.children[0].name).toBe('Asia');
+        expect(menuItem.children[0].name).toBe('asia');
     });
 });
